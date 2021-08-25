59b08a5eac448ba9d0f2c1d8f9a140d96fb28f33
Batyrkhan <aytp84@gmail.com>
2021-08-25 20:16:38 +0500

    starting php

--- form.php
+++ form.php
@@ -8,16 +8,16 @@
 
 <body>
     <h1><?php echo "Hello World" ?></h1>
-    <!--<?php
+<?php
 // Сообщение
-$message = "Line 1\r\nLine 2\r\nLine 3";
+//$message = "Line 1\r\nLine 2\r\nLine 3";
 
 // На случай если какая-то строка письма длиннее 70 символов мы используем wordwrap()
-$message = wordwrap($message, 70, "\r\n");
+//$message = wordwrap($message, 70, "\r\n");
 
 // Отправляем
-mail('aytp84@gmail.com', 'My Subject', $message);
-?>-->
+//mail('aytp84@gmail.com', 'My Subject', $message);
+?>
 
 </body>
 </html>
